24e6172ac3c4a835161156fba3f00d6f3625efed
adrian mendez <eladri-@live.com>
2020-11-03 20:02:10 -0400

    Add project files.

---
+++ AdrianSegundoParcialAP2/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdrianSegundoParcialAP2.Models {
+    public class Cliente {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+    }
+}
---
+++ AdrianSegundoParcialAP2/Models/Venta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdrianSegundoParcialAP2.Models {
+    public class Venta {
+
+        [Key]
+        public int VentaId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int ClienteId { get; set; }
+        public double Monto { get; set; }
+        public double Balance { get; set; }
+
+    }
+}
